1332381c75700ae042402e6f3d1c1e2fb9698862
Pierre LAMOUR <pierrelamournew@hotmail.com>
2024-10-29 13:57:17 +0100

    Commit

--- src/main/java/net/earlyalpha/aristysa/block/custom/CraftStationBlock.java
+++ src/main/java/net/earlyalpha/aristysa/block/custom/CraftStationBlock.java
@@ -20,7 +20,7 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class CraftStationBlock extends BlockWithEntity implements BlockEntityProvider {
-    private static final VoxelShape SHAPE = Block.createCuboidShape(0,0,0,16,17,32);
+    private static final VoxelShape SHAPE = Block.createCuboidShape(0,0,0,16,16,16);
     public CraftStationBlock(Settings settings) {
         super(settings);
     }
